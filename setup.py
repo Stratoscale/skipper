e9bc005523df321957a9aa2e7d8fa9d96481b59f
Eran <eran@stratoscale.com>
2016-08-17 15:22:59 +0300

    Adde entry_point in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages
-from setuptools import setup
-import os
-import subprocess
+from distutils.core import setup
 
 setup(
     name='skipper',
-    version='0.0.1',
+    version='0.0.4',
     url='http://github.com/Stratoscale/skipper',
     author='Adir Gabai',
     author_mail='adir@stratoscale.com',
-    packages=find_packages(include=['skipper*'])
+    packages=find_packages(include=['skipper*']),
+    entry_points={
+          'console_scripts': [
+              'skipper = skipper.main:main',
+          ],
+      },
 )
