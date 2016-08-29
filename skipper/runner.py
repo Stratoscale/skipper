ffa3d5fbe284dae61851b5424ed4f171f0e6686b
Adir Gabai <adir@stratoscale.com>
2016-08-29 14:06:59 +0300

    Added runner module for running commands inside and outside a container.

---
+++ skipper/runner.py
@@ -0,0 +1,55 @@
+import grp
+import logging
+import os
+import subprocess
+
+
+def run(command, fqdn_image=None, environment=None):
+    if fqdn_image is not None:
+        cwd = os.getcwd()
+        workspace = os.path.dirname(cwd)
+        project = os.path.basename(cwd)
+
+        uid = os.getuid()
+        gid = grp.getgrnam('docker').gr_gid
+        _run_nested(workspace, project, uid, gid, fqdn_image, environment, command)
+    else:
+        _run(command)
+
+
+def _run(cmd):
+    logging.debug(" ".join(cmd))
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    output = []
+    while proc.poll() is None:
+        line = proc.stdout.readline()
+        output.append(line.rstrip())
+        logging.debug(line.rstrip())
+
+    return output
+
+
+def _run_nested(workspace, project, uid, gid, fqdn_image, environment, command):
+    docker_cmd = ["docker", "run"]
+    docker_cmd += ["--rm"]
+    docker_cmd += ["--net", "host"]
+
+    environment = environment or []
+    for env in environment:
+        docker_cmd += ["-e", env]
+
+    volumes = [
+        "%(workspace)s:/workspace:rw,Z" % dict(workspace=workspace),
+        "/var/lib/osmosis:/var/lib/osmosis:rw,Z" % dict(workspace=workspace),
+        "/var/run/docker.sock:/var/run/docker.sock:Z",
+    ]
+    for volume in volumes:
+        docker_cmd += ["-v", volume]
+
+    docker_cmd += ["-u", "%(uid)d:%(gid)d" % dict(uid=uid, gid=gid)]
+    docker_cmd += ["-w", "%(workdir)s" % dict(workdir=os.path.join("/workspace", project))]
+    docker_cmd += ["--entrypoint", command[0]]
+    docker_cmd += [fqdn_image]
+
+    return _run(docker_cmd + command[1:])
---
+++ tests/test_runner.py
@@ -0,0 +1,149 @@
+import mock
+import os
+import subprocess
+import unittest
+from skipper import runner
+
+
+USER_ID = 1000
+GROUP_ID = 2000
+
+REGISTRY = 'registry.io:5000'
+IMAGE = 'image'
+TAG = '1234567'
+FQDN_IMAGE = REGISTRY + '/' + IMAGE + ':' + TAG
+
+WORKDIR = '/home/adir/work'
+PROJECT = 'proj'
+PROJECT_DIR = os.path.join(WORKDIR, PROJECT)
+
+ENV = ["KEY1=VAL1", "KEY2=VAL2"]
+
+
+class TestRunner(unittest.TestCase):
+    @mock.patch('subprocess.Popen', autospec=False)
+    def test_run_simple_command_not_nested(self, popen_mock):
+        popen_mock.return_value.poll.side_effect = [None, None, None, -1]
+        command = ['pwd']
+        runner.run(command)
+        popen_mock.assert_called_once_with(
+            command,
+            stdout=subprocess.PIPE
+        )
+
+    @mock.patch('subprocess.Popen', autospec=False)
+    def test_run_complex_command_not_nested(self, popen_mock):
+        command = ['ls -l']
+        runner.run(command)
+        popen_mock.assert_called_once_with(
+            command,
+            stdout=subprocess.PIPE
+        )
+
+    @mock.patch('os.getuid', autospec=True, return_value=USER_ID)
+    @mock.patch('os.getcwd', autospec=True, return_value=PROJECT_DIR)
+    @mock.patch('grp.getgrnam', autospec=True,)
+    @mock.patch('subprocess.Popen', autospec=False)
+    def test_run_simple_command_nested(self, popen_mock, getgrnam_mock, *args):
+        popen_mock.return_value.poll.side_effect = [None, None, None, -1]
+        command = ['pwd']
+        getgrnam_mock.return_value.gr_gid = GROUP_ID
+        runner.run(command, FQDN_IMAGE)
+        expected_nested_command = [
+            'docker', 'run',
+            '--rm',
+            '--net', 'host',
+            '-v', '%(workdir)s:/workspace:rw,Z' % dict(workdir=WORKDIR),
+            '-v', '/var/lib/osmosis:/var/lib/osmosis:rw,Z',
+            '-v', '/var/run/docker.sock:/var/run/docker.sock:Z',
+            '-u', '%(uid)s:%(gid)s' % dict(uid=USER_ID, gid=GROUP_ID),
+            '-w', '/workspace/proj',
+            '--entrypoint', command[0],
+            FQDN_IMAGE
+        ]
+        popen_mock.assert_called_once_with(
+            expected_nested_command,
+            stdout=subprocess.PIPE
+        )
+
+    @mock.patch('os.getuid', autospec=True, return_value=USER_ID)
+    @mock.patch('os.getcwd', autospec=True, return_value=PROJECT_DIR)
+    @mock.patch('grp.getgrnam', autospec=True,)
+    @mock.patch('subprocess.Popen', autospec=False)
+    def test_run_simple_command_nested_with_env(self, popen_mock, getgrnam_mock, *args):
+        popen_mock.return_value.poll.side_effect = [None, None, None, -1]
+        command = ['pwd']
+        getgrnam_mock.return_value.gr_gid = GROUP_ID
+        runner.run(command, FQDN_IMAGE, ENV)
+        expected_docker_command = [
+            'docker', 'run',
+            '--rm',
+            '--net', 'host',
+            '-e', 'KEY1=VAL1',
+            '-e', 'KEY2=VAL2',
+            '-v', '%(workdir)s:/workspace:rw,Z' % dict(workdir=WORKDIR),
+            '-v', '/var/lib/osmosis:/var/lib/osmosis:rw,Z',
+            '-v', '/var/run/docker.sock:/var/run/docker.sock:Z',
+            '-u', '%(uid)s:%(gid)s' % dict(uid=USER_ID, gid=GROUP_ID),
+            '-w', '/workspace/proj',
+            '--entrypoint', command[0],
+            FQDN_IMAGE
+        ]
+        popen_mock.assert_called_once_with(
+            expected_docker_command,
+            stdout=subprocess.PIPE
+        )
+
+    @mock.patch('os.getuid', autospec=True, return_value=USER_ID)
+    @mock.patch('os.getcwd', autospec=True, return_value=PROJECT_DIR)
+    @mock.patch('grp.getgrnam', autospec=True,)
+    @mock.patch('subprocess.Popen', autospec=False)
+    def test_run_complex_command_nested(self, popen_mock, getgrnam_mock, *args):
+        command = ['ls', '-l']
+        getgrnam_mock.return_value.gr_gid = GROUP_ID
+        runner.run(command, FQDN_IMAGE)
+        expected_nested_command = [
+            'docker', 'run',
+            '--rm',
+            '--net', 'host',
+            '-v', '%(workdir)s:/workspace:rw,Z' % dict(workdir=WORKDIR),
+            '-v', '/var/lib/osmosis:/var/lib/osmosis:rw,Z',
+            '-v', '/var/run/docker.sock:/var/run/docker.sock:Z',
+            '-u', '%(uid)s:%(gid)s' % dict(uid=USER_ID, gid=GROUP_ID),
+            '-w', '/workspace/proj',
+            '--entrypoint', command[0],
+            FQDN_IMAGE,
+            command[1]
+        ]
+        popen_mock.assert_called_once_with(
+            expected_nested_command,
+            stdout=subprocess.PIPE
+        )
+
+    @mock.patch('os.getuid', autospec=True, return_value=USER_ID)
+    @mock.patch('os.getcwd', autospec=True, return_value=PROJECT_DIR)
+    @mock.patch('grp.getgrnam', autospec=True,)
+    @mock.patch('subprocess.Popen', autospec=False)
+    def test_run_complex_command_nested_with_env(self, popen_mock, getgrnam_mock, *args):
+        command = ['ls', '-l']
+        getgrnam_mock.return_value.gr_gid = GROUP_ID
+        runner.run(command, FQDN_IMAGE, ENV)
+        expected_nested_command = [
+            'docker', 'run',
+            '--rm',
+            '--net', 'host',
+            '-e', 'KEY1=VAL1',
+            '-e', 'KEY2=VAL2',
+            '-v', '%(workdir)s:/workspace:rw,Z' % dict(workdir=WORKDIR),
+            '-v', '/var/lib/osmosis:/var/lib/osmosis:rw,Z',
+            '-v', '/var/run/docker.sock:/var/run/docker.sock:Z',
+            '-u', '%(uid)s:%(gid)s' % dict(uid=USER_ID, gid=GROUP_ID),
+            '-w', '/workspace/proj',
+            '--entrypoint', command[0],
+            FQDN_IMAGE,
+            command[1]
+        ]
+        popen_mock.assert_called_once_with(
+            expected_nested_command,
+            stdout=subprocess.PIPE
+        )
