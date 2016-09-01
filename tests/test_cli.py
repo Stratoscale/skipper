7c245808fd47f7137624c81f0337d1f964f111c1
Adir Gabai <adir@stratoscale.com>
2016-09-01 13:28:13 +0300

    Refactored CLI tests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -31,37 +31,53 @@ class TestCLI(unittest.TestCase):
         result = self._invoke_cli()
         self.assertEqual(result.exit_code, 0)
 
-    def test_help(self):
-        result = self._invoke_cli(['--help'])
+    def test_cli_help(self):
+        result = self._invoke_cli(global_params=['--help'])
         self.assertEqual(result.exit_code, 0)
 
+    def test_subcommand_help(self):
         for subcmd in ('build', 'push', 'make', 'run'):
-            result = self._invoke_cli(None, subcmd, ['--help'])
+            result = self._invoke_cli(
+                global_params=None,
+                subcmd=subcmd,
+                subcmd_params=['--help']
+            )
             self.assertEqual(result.exit_code, 0)
 
-    def test_nested_mode_without_global_parameters(self):
-        result = self._invoke_cli(None, 'build', [IMAGE])
-        self.assertIsInstance(result.exception, click.BadParameter)
-        self.assertEqual(result.exit_code, -1)
+    def test_subcommand_without_global_params(self):
+        subcmd_params_map = {
+            'build': [IMAGE],
+            'push': [IMAGE],
+            'run': ['ls' '-l'],
+            'make': ['-f', 'Makefile', 'all'],
+        }
 
-        result = self._invoke_cli(None, 'push', [IMAGE])
-        self.assertIsInstance(result.exception, click.BadParameter)
-        self.assertEqual(result.exit_code, -1)
+        for subcmd, subcmd_params in subcmd_params_map.iteritems():
+            result = self._invoke_cli(
+                global_params=None,
+                subcmd=subcmd,
+                subcmd_params=subcmd_params,
+            )
+            self.assertIsInstance(result.exception, click.BadParameter)
+            self.assertEqual(result.exit_code, -1)
 
-        result = self._invoke_cli(None, 'run', ['ls', '-l'])
-        self.assertIsInstance(result.exception, click.BadParameter)
-        self.assertEqual(result.exit_code, -1)
-
-        result = self._invoke_cli(None, 'make', ['-f', 'Makefile', 'all'])
-        self.assertIsInstance(result.exception, click.BadParameter)
-        self.assertEqual(result.exit_code, -1)
+    @mock.patch('skipper.runner.run', autospec=True)
+    def test_subcommand_without_subcommand_params(self, skipper_runner_run_mock):
+        for subcmd in ('build', 'push', 'run', 'make'):
+            result = self._invoke_cli(self.global_params, subcmd)
+            self.assertNotEqual(result.exit_code, 0)
+            self.assertFalse(skipper_runner_run_mock.called)
 
     @mock.patch('skipper.git.get_hash', autospec=True, return_value=TAG)
     @mock.patch('skipper.runner.run', autospec=True)
     def test_build(self, skipper_runner_run_mock, *args):
         dockerfile = IMAGE + '.Dockerfile'
         build_params = [IMAGE]
-        self._invoke_cli(self.global_params, 'build', build_params)
+        self._invoke_cli(
+            global_params=self.global_params,
+            subcmd='build',
+            subcmd_params=build_params
+        )
         expected_command = [
             'docker',
             'build',
@@ -76,7 +92,11 @@ class TestCLI(unittest.TestCase):
     def test_push(self, skipper_runner_run_mock, *args):
         dockerfile = IMAGE + '.Dockerfile'
         push_params = [IMAGE]
-        self._invoke_cli(self.global_params, 'push', push_params)
+        self._invoke_cli(
+            global_params=self.global_params,
+            subcmd='push',
+            subcmd_params=push_params
+        )
         expected_command = [
             'docker',
             'push',
@@ -85,23 +105,25 @@ class TestCLI(unittest.TestCase):
         skipper_runner_run_mock.assert_called_once_with(expected_command, fqdn_image=BUILD_CONTAINER_FQDN_IMAGE)
 
     @mock.patch('skipper.runner.run', autospec=True)
-    def test_run_without_command(self, skipper_runner_run_mock):
-        result = self._invoke_cli(self.global_params, 'run')
-        self.assertNotEqual(result.exit_code, 0)
-        self.assertFalse(skipper_runner_run_mock.called)
-
-    @mock.patch('skipper.runner.run', autospec=True)
     def test_run(self, skipper_runner_run_mock):
         command = ['ls', '-l']
         run_params = command
-        self._invoke_cli(self.global_params, 'run', run_params)
+        self._invoke_cli(
+            global_params=self.global_params,
+            subcmd='run',
+            subcmd_params=run_params
+        )
         skipper_runner_run_mock.assert_called_once_with(command, fqdn_image=BUILD_CONTAINER_FQDN_IMAGE, environment=[])
 
     @mock.patch('skipper.runner.run', autospec=True)
     def test_run_with_env(self, skipper_runner_run_mock):
         command = ['ls', '-l']
         run_params = ['-e', ENV[0], '-e', ENV[1]] + command
-        self._invoke_cli(self.global_params, 'run', run_params)
+        self._invoke_cli(
+            global_params=self.global_params,
+            subcmd='run',
+            subcmd_params=run_params
+        )
         skipper_runner_run_mock.assert_called_once_with(command, fqdn_image=BUILD_CONTAINER_FQDN_IMAGE, environment=ENV)
 
     @mock.patch('skipper.runner.run', autospec=True)
@@ -109,12 +131,18 @@ class TestCLI(unittest.TestCase):
         makefile = 'Makefile'
         target = 'all'
         make_params = ['-f', makefile, target]
-        self._invoke_cli(self.global_params, 'make', make_params)
+        self._invoke_cli(
+            global_params=self.global_params,
+            subcmd='make',
+            subcmd_params=make_params
+        )
         expected_command = ['make', '-f', makefile, target]
         skipper_runner_run_mock.assert_called_once_with(expected_command, fqdn_image=BUILD_CONTAINER_FQDN_IMAGE, environment=[])
 
-    def _invoke_cli(self, global_params=None, subcmd=None, subcmd_params=None):
+    def _invoke_cli(self, defaults=None, global_params=None, subcmd=None, subcmd_params=None):
         self.assertFalse(subcmd is None and subcmd_params is not None, 'No sub-command was provided!')
+
+        defaults = defaults or {}
 
         cli_params = []
         if global_params is not None:
@@ -126,4 +154,4 @@ class TestCLI(unittest.TestCase):
         if subcmd_params is not None:
             cli_params += subcmd_params
 
-        return self._runner.invoke(cli.cli, cli_params, obj={}, standalone_mode=False)
+        return self._runner.invoke(cli.cli, cli_params, default_map=defaults, obj={}, standalone_mode=False)
