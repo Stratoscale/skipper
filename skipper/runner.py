9ccbba274047b2b86545017265439e0495ad6ef2
Adir Gabai <adir@stratoscale.com>
2016-09-08 09:01:04 +0300

    Surround strings with '' instead of "" (for consistency purposes).

--- skipper/runner.py
+++ skipper/runner.py
@@ -34,26 +34,26 @@ def _run(cmd):
 
 
 def _run_nested(workspace, project, uid, gid, fqdn_image, environment, command):
-    docker_cmd = ["docker", "run"]
-    docker_cmd += ["-t"]
-    docker_cmd += ["--rm"]
-    docker_cmd += ["--net", "host"]
+    docker_cmd = ['docker', 'run']
+    docker_cmd += ['-t']
+    docker_cmd += ['--rm']
+    docker_cmd += ['--net', 'host']
 
     environment = environment or []
     for env in environment:
-        docker_cmd += ["-e", env]
+        docker_cmd += ['-e', env]
 
     volumes = [
-        "%(workspace)s:/workspace:rw,Z" % dict(workspace=workspace),
-        "/var/lib/osmosis:/var/lib/osmosis:rw,Z" % dict(workspace=workspace),
-        "/var/run/docker.sock:/var/run/docker.sock:Z",
+        '%(workspace)s:/workspace:rw,Z' % dict(workspace=workspace),
+        '/var/lib/osmosis:/var/lib/osmosis:rw,Z' % dict(workspace=workspace),
+        '/var/run/docker.sock:/var/run/docker.sock:Z',
     ]
     for volume in volumes:
-        docker_cmd += ["-v", volume]
+        docker_cmd += ['-v', volume]
 
-    docker_cmd += ["-u", "%(uid)d:%(gid)d" % dict(uid=uid, gid=gid)]
-    docker_cmd += ["-w", "%(workdir)s" % dict(workdir=os.path.join("/workspace", project))]
-    docker_cmd += ["--entrypoint", command[0]]
+    docker_cmd += ['-u', '%(uid)d:%(gid)d' % dict(uid=uid, gid=gid)]
+    docker_cmd += ['-w', '%(workdir)s' % dict(workdir=os.path.join('/workspace', project))]
+    docker_cmd += ['--entrypoint', command[0]]
     docker_cmd += [fqdn_image]
 
     return _run(docker_cmd + command[1:])
