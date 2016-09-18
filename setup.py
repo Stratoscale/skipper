5ae74d6770e73a5613a96d73228d166528a291fa
Adir Gabai <adir@stratoscale.com>
2016-09-18 10:42:24 +0300

    Moved to pbr.

--- setup.py
+++ setup.py
@@ -3,33 +3,10 @@ from setuptools import find_packages
 from distutils.core import setup
 
 
-
-data_files = []
-# Install data files only when installing as root
-if os.getuid() == 0:
-    data_files=[
-        ('/etc/bash_completion.d', ['data/skipper-complete.sh']),
-        ('/opt/skipper', ['data/skipper-entrypoint.sh']),
-    ]
-
 setup(
-    name='strato-skipper',
-    version='1.0.1',
-    url='http://github.com/Stratoscale/skipper',
-    author='Adir Gabai',
-    author_email='adir@stratoscale.com',
-    description='Easily dockerize your Git repository',
-    packages=find_packages(include=['skipper*']),
-    data_files=data_files,
-    entry_points={
-          'console_scripts': [
-              'skipper = skipper.main:main',
-          ],
-      },
-    install_requires=[
-            'PyYAML>=3.11',
-            'click>=6.6',
-            'requests>=2.6.0',
-            'tabulate>=0.7.5',
-        ]
+    setup_requires = [
+        'pbr >= 1.9',
+        'setuptools >= 17.1'
+    ],
+    pbr = True
 )
