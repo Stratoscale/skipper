18369940730d0e276c1c294f87d5c8ac00de0951
Adir Gabai <adir@stratoscale.com>
2016-09-25 13:13:51 +0300

    Don't allow wheel distribution. Wheel distribution doesn't support absolute paths for data_files (see: https://bitbucket.org/pypa/wheel/issues/92)

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import os
+import sys
 from setuptools import find_packages
 from distutils.core import setup
 
+# Workaround: bdist_wheel doesn't support absolute paths in data_files
+# (see: https://bitbucket.org/pypa/wheel/issues/92). 
+if os.getuid() == 0 and 'bdist_wheel' in sys.argv:
+    raise RuntimeError("This setup.py does not support wheels")
 
 setup(
     setup_requires = [
