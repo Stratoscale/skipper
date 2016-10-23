a217748cd2985658d6e3aabe9e322cbd785b419f
Adir Gabai <adir@stratoscale.com>
2016-10-23 11:52:51 +0300

    Create docker group inside the build container even if there is already a group with the same GID.

--- data/skipper-entrypoint.sh
+++ data/skipper-entrypoint.sh
@@ -5,7 +5,7 @@ if [ x"$?" != x"0" ]; then
 	useradd -u ${SKIPPER_UID} --non-unique -M "${SKIPPER_USERNAME}"
 fi
 
-groupadd -g ${SKIPPER_DOCKER_GID} docker
+groupadd -g ${SKIPPER_DOCKER_GID} --non-unique docker
 usermod -G root,docker ${SKIPPER_USERNAME}
 
 su -m ${SKIPPER_USERNAME} -c "$@"
