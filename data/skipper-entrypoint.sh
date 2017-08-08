edb293d3be9625e89a0192878b1a97a081818457
Michael Filanov <michael.filanov@stratoscale.com>
2017-08-08 14:59:38 +0300

    change docker group id if exists

--- data/skipper-entrypoint.sh
+++ data/skipper-entrypoint.sh
@@ -5,7 +5,12 @@ if [ x"$?" != x"0" ]; then
 	useradd -u ${SKIPPER_UID} --non-unique -M "${SKIPPER_USERNAME}"
 fi
 
-groupadd -g ${SKIPPER_DOCKER_GID} --non-unique docker
+if [ $(getent group docker) ]; then
+	groupmod -o -g ${SKIPPER_DOCKER_GID} docker
+else
+	groupadd -g ${SKIPPER_DOCKER_GID} --non-unique docker
+fi
+
 usermod -G root,docker ${SKIPPER_USERNAME}
 
 su -m ${SKIPPER_USERNAME} -c "$@"
