2179e3fee0307d697bb3298a0d0ee6be6bebc674
Ygal Blum <ygal.blum@stratoscale.com>
2018-07-17 11:53:27 -0400

    setup.py use setuptools instead of distutils.core
    When setuptools installs all dependencies as part of the package installation
    while distutils silently fails the installation

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 import os
 import sys
 from setuptools import find_packages
-from distutils.core import setup
 
 # Workaround: bdist_wheel doesn't support absolute paths in data_files
 # (see: https://bitbucket.org/pypa/wheel/issues/92). 
 if os.getuid() == 0 and 'bdist_wheel' in sys.argv:
     raise RuntimeError("This setup.py does not support wheels")
 
-setup(
+setuptools.setup(
     setup_requires=[
         'pbr >= 1.9',
         'setuptools >= 17.1'
