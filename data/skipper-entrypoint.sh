4e805c5eacd6a929707825c0828f173fdfa3afc1
Ronnie Lazar <ronnie@stratoscale.com>
2019-08-26 16:39:19 +0300

    Minor review comments changes for the home dir handling

--- data/skipper-entrypoint.sh
+++ data/skipper-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 SKIP_HOME_DIR_PARAM=""
-# if home directory for user already exists, then useradd should not try to create it,
-# but we'll need to make sure that the user is owner of it
-[ -d /home/${SKIPPER_USERNAME} ] && SKIP_HOME_DIR_PARAM="-M"
+# if home directory already exists, useradd should not try to create it
+[ -d /home/${SKIPPER_USERNAME} ] && SKIP_HOME_DIR_PARAM="--no-create-home"
 
 getent passwd ${SKIPPER_USERNAME} > /dev/null
 if [ x"$?" != x"0" ]; then
