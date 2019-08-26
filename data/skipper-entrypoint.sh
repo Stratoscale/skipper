ec242d04f5cede3c47f63b78d578301677127e9c
Ronnie Lazar <ronnie@stratoscale.com>
2019-08-26 15:50:50 +0300

    Handle home dir permissions

--- data/skipper-entrypoint.sh
+++ data/skipper-entrypoint.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+SKIP_HOME_DIR_PARAM=""
+# if home directory for user already exists, then useradd should not try to create it,
+# but we'll need to make sure that the user is owner of it
+[ -d /home/${SKIPPER_USERNAME} ] && SKIP_HOME_DIR_PARAM="-M"
+
 getent passwd ${SKIPPER_USERNAME} > /dev/null
 if [ x"$?" != x"0" ]; then
-	useradd -u ${SKIPPER_UID} --non-unique -M "${SKIPPER_USERNAME}"
+	useradd -u ${SKIPPER_UID} --non-unique ${SKIP_HOME_DIR_PARAM} "${SKIPPER_USERNAME}"
 fi
+
+chown ${SKIPPER_USERNAME}:${SKIPPER_USERNAME} /home/${SKIPPER_USERNAME}
 
 if [ $(getent group docker) ]; then
 	groupmod -o -g ${SKIPPER_DOCKER_GID} docker
