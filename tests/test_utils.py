df7c3b0ff552f138157cb506f6ecfa10788b2cd1
Igal Tsoiref <itsoiref@redhat.com>
2020-06-15 17:42:34 +0300

    Adding special treatment for docker/config and .gitconfig files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,3 +24,27 @@ class TestUtils(unittest.TestCase):
         utils.CONTAINER_RUNTIME_COMMAND = utils.DOCKER
         res = utils.get_runtime_command()
         self.assertEquals(res, utils.DOCKER)
+
+    @mock.patch('skipper.utils.open', autospec=False)
+    @mock.patch('skipper.utils.os.makedirs', autospec=True)
+    @mock.patch('skipper.utils.os.path.exists', autospec=True)
+    def test_create_path_and_add_data(self, path_exists_mock, makedir_mock, open_mock):
+        test_dir = "/home/test"
+        test_file = os.path.join(test_dir, "test_file.txt")
+        path_exists_mock.return_value = False
+        utils.create_path_and_add_data(test_file, "", False)
+        makedir_mock.assert_called_once_with(test_dir)
+        open_mock.assert_not_called()
+
+        makedir_mock.reset_mock()
+        test_file = os.path.join(test_dir, "test_file.txt")
+        path_exists_mock.return_value = True
+        utils.create_path_and_add_data(test_file, "", False)
+        makedir_mock.assert_not_called()
+        open_mock.assert_not_called()
+
+        test_file = os.path.join(test_dir, "test_file.txt")
+        path_exists_mock.return_value = True
+        utils.create_path_and_add_data(test_file, "", True)
+        makedir_mock.assert_not_called()
+        open_mock.assert_called_once_with(test_file, "w")
