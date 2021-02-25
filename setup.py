0741b5719181d316b951f8839183af19d06a5d17
Eran Cohen <eranco@redhat.com>
2021-02-25 13:47:27 +0200

    Moved project description to setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import io
 import os
 import sys
 from setuptools import setup
@@ -8,17 +7,10 @@ from setuptools import setup
 if os.getuid() == 0 and 'bdist_wheel' in sys.argv:
     raise RuntimeError("This setup.py does not support wheels")
 
-# read the contents the README file
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with io.open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     setup_requires=[
         'pbr >= 1.9',
         'setuptools >= 17.1'
     ],
     pbr=True,
-    long_description=long_description,
-    long_description_content_type='text/markdown'
 )
