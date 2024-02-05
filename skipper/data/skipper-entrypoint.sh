9a17bb1b28a962c56c96cd83e6f971b515d78439
Anton Khizunov <anton.khizunov@zadarastorage.com>
2024-02-05 09:10:12 +0000

    Fix sudo command in skipper-entrypoint.sh

--- skipper/data/skipper-entrypoint.sh
+++ skipper/data/skipper-entrypoint.sh
@@ -28,10 +28,10 @@ if ! [ -z "${SKIPPER_DOCKER_GID}" ];then
      usermod -G root ${SKIPPER_USERNAME}
   fi
 
-  if sudo -l -U ${SKIPPER_USERNAME} > /dev/null; then
+  if sudo -l -U ${SKIPPER_USERNAME} 2> /dev/null; then
     # for debian distros (maybe for others too) -m flag resets the PATH variable
     # so we need to use sudo -E to preserve the PATH
-    sudo -sE -u ${SKIPPER_USERNAME} "$@"
+    sudo -sE -u ${SKIPPER_USERNAME} $@
   else 
     su -m ${SKIPPER_USERNAME} -c "$@"
   fi
